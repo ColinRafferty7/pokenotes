5ec97507f7375ea7be7a8d6b8afdfdd8ed541464
ColinRafferty7 <ctr6mince@gmail.com>
2025-06-11 01:02:41 -0400

    Adding notes screen with left click pop up

--- PokeNotes/Assets/Scripts/StartButton.cs
+++ PokeNotes/Assets/Scripts/StartButton.cs
@@ -1,9 +1,10 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StartButton : MonoBehaviour
 {
     public void StartButtonClick()
     {
-        Debug.Log("Button");
+        SceneManager.LoadScene("Notes");
     }
 }
